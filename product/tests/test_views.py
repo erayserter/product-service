b520ca63f8db763c64c65999759b947a9306af28
Mehmet Eray Serter <mehmeterayserter@gmail.com>
2023-09-19 00:11:49 +0300

    tests added for duplicate code field

--- product/tests/test_views.py
+++ product/tests/test_views.py
@@ -54,6 +54,18 @@ class CreateViewTestCase(BaseProductAPITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data.get('code'), self.data['code'])
 
+    def test_create_view_duplicate_code(self):
+        url = reverse('products')
+
+        self.data['code'] = self.data.get('name').replace(' ', '-')
+
+        response_first = self.client.post(url, self.data, format='json')
+        response_second = self.client.post(url, self.data, format='json')
+
+        self.assertEqual(response_first.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response_first.data.get('code'), self.data['code'])
+        self.assertEqual(response_second.status_code, status.HTTP_400_BAD_REQUEST)
+
     def test_create_view_without_auth(self):
         self.client.logout()
 
